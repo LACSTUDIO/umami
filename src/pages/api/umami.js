52dc5a9d68411916ade677818bc43b953644d644
领创工作室 <105140272+LACSTUDIO@users.noreply.github.com>
2025-02-19 23:16:45 +0800

    Update umami.js

--- src/pages/api/umami.js
+++ src/pages/api/umami.js
@@ -1,5 +1,16 @@
 // api/umami.js
 export default async function handler(req, res) {
+  // 设置 CORS 头
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.xn--5brr03o.top/');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // 如果是 OPTIONS 请求，直接返回
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   try {
     const { query: { websiteId }, method } = req;
 
@@ -7,7 +18,7 @@ export default async function handler(req, res) {
       return res.status(405).json({ error: 'Method not allowed' });
     }
 
-    const token = process.env.UMAMI_TOKEN; // 确保环境变量名称正确
+    const token = process.env.UMAMI_TOKEN; // 从环境变量中获取 token
     const umiId = websiteId || 'a4e8c20f-d2e8-4b10-bdf5-2d52c389fd45';
     const umiTime = Date.now();
 
