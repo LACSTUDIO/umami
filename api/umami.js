ed62222e72b2d19d5244b568dfd9d0f86dfb69b2
领创工作室 <105140272+LACSTUDIO@users.noreply.github.com>
2025-02-19 22:07:15 +0800

    Create umami.js

---
+++ api/umami.js
@@ -0,0 +1,29 @@
+// api/umami.js
+export default async function handler(req, res) {
+  try {
+    const { query: { websiteId }, method } = req;
+
+    if (method !== 'GET') {
+      return res.status(405).json({ error: 'Method not allowed' });
+    }
+
+    const token = process.env.UMAMI_TOKEN;
+    const umiId = websiteId || 'a4e8c20f-d2e8-4b10-bdf5-2d52c389fd45';
+    const umiTime = Date.now();
+
+    const umiUrl = `https://umami.xn--5brr03o.top/api/websites/${umiId}/stats?startAt=0000000000&endAt=${umiTime}`;
+
+    const response = await fetch(umiUrl, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: `API Error: ${error.message}` });
+  }
+}
